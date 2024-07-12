c90cb094d12a6f44e7bc333df246d995455c5ea8
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-12 20:43:42 +0200

    feat: add main and single product

--- src/component/AppMain.vue
+++ src/component/AppMain.vue
@@ -1,15 +1,27 @@
 <script>
+import SingleProduct from './SingleProduct.vue';
     export default {
-        name: 'AppMain'
+        name: 'AppMain',
+        components: {
+            SingleProduct
+        }
     }
 </script>
 
 <template>
+    <main>
+        <div class="row">
+            <SingleProduct />
+        </div>
+    </main>
 
 
 </template>
 
 <style lang="scss" scoped>
-
+    .row {
+        background-color: #1b1b1b;
+        min-height: 800px;
+    }
 
 </style>
---
+++ src/component/SingleProduct.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        name: 'SingleProduct'
+    }
+</script>
+
+<template>
+    <div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+div {
+    color: white;
+}
+
+</style>
